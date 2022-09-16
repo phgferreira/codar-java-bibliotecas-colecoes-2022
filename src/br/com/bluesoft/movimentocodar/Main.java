a483194103bf8e155a10faffd4b22e744ebef98f
Paulo Ferreira <phgferreira@outlook.com>
2022-09-15 23:11:17 -0300

    Recebendo um valor qualquer do console

--- src/br/com/bluesoft/movimentocodar/Main.java
+++ src/br/com/bluesoft/movimentocodar/Main.java
@@ -1,9 +1,17 @@
 package br.com.bluesoft.movimentocodar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		while(true) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String opcao = reader.readLine();
+			System.out.println(opcao);
+		}
 	}
 
 }
