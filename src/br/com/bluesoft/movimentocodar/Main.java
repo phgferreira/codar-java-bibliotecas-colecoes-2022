b65bf90cddfdd9cb8c6618bbfc095d6249e1fd52
Paulo Ferreira <phgferreira@outlook.com>
2022-09-15 23:24:41 -0300

    Usando um Scanner só para tratar quando digitar não usando número

--- src/br/com/bluesoft/movimentocodar/Main.java
+++ src/br/com/bluesoft/movimentocodar/Main.java
@@ -1,15 +1,15 @@
 package br.com.bluesoft.movimentocodar;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		while(true) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-			String opcao = reader.readLine();
+			Scanner reader = new Scanner(new InputStreamReader(System.in));
+			int opcao = reader.nextInt();
 			System.out.println(opcao);
 		}
 	}
