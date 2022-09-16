e1e89f5ebbb3291d6579b4fa35172a33f5ef0809
Paulo Ferreira <phgferreira@outlook.com>
2022-09-16 19:46:47 -0300

    Fazendo a leitura das perguntas que estão no arquivo com Scanner

--- src/br/com/bluesoft/movimentocodar/Main.java
+++ src/br/com/bluesoft/movimentocodar/Main.java
@@ -2,15 +2,16 @@ package br.com.bluesoft.movimentocodar;
 
 import java.io.IOException;
 
-import br.com.bluesoft.movimentocodar.menu.MenuPrincipal;
+import br.com.bluesoft.movimentocodar.menu.MenuCandidatarSe;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		while(true) {
-			new MenuPrincipal().abreMenu();
-			System.out.println("--- Volta ao MenuPrincipal ---");
-		}
+		new MenuCandidatarSe().abreMenu();
+//		while(true) {
+//			new MenuPrincipal().abreMenu();
+//			System.out.println("--- Volta ao MenuPrincipal ---");
+//		}
 	}
 
 }
--- src/br/com/bluesoft/movimentocodar/menu/MenuCandidatarSe.java
+++ src/br/com/bluesoft/movimentocodar/menu/MenuCandidatarSe.java
@@ -1,5 +1,9 @@
 package br.com.bluesoft.movimentocodar.menu;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MenuCandidatarSe implements Menu {
 
 	@Override
@@ -10,6 +14,15 @@ public class MenuCandidatarSe implements Menu {
 	@Override
 	public void abreMenu() {
 		System.out.println(">>> " + this.getTitulo() + " <<<");
+		
+		try {
+			Scanner scanner = new Scanner( new File("formulario.txt") );
+			scanner.useDelimiter("\\|");
+			while (scanner.hasNext())
+				System.out.println(scanner.next());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
