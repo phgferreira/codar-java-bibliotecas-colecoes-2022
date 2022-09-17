392b9565f545f3b89435c83e9f44b7f568ddac09
Paulo Ferreira <phgferreira@outlook.com>
2022-09-16 22:09:17 -0300

    Lê arquivo de perguntas, realiza as perguntas e coleta as respostas

--- src/br/com/bluesoft/movimentocodar/menu/MenuCandidatarSe.java
+++ src/br/com/bluesoft/movimentocodar/menu/MenuCandidatarSe.java
@@ -1,17 +1,23 @@
 package br.com.bluesoft.movimentocodar.menu;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import br.com.bluesoft.movimentocodar.modelo.PerguntaResposta;
 
+/*
+ * pr = PerguntaResposta
+ */
 public class MenuCandidatarSe implements Menu {
-
-	private List<PerguntaResposta> perguntasERespostas;
 	
+	private List<PerguntaResposta> perguntaRespostas;
+
 	@Override
 	public String getTitulo() {
 		return "Candidatar-se";
@@ -20,26 +26,32 @@ public class MenuCandidatarSe implements Menu {
 	@Override
 	public void abreMenu() {
 		System.out.println(">>> " + this.getTitulo() + " <<<");
-		exibePerguntas();
-	}
-	
-	private void exibePerguntas() {
 		try {
-
-			Scanner scanner = new Scanner( new File("formulario.txt") );
-			// Usa o '|' e a quebr de linha como delimitador
-			scanner.useDelimiter( "\\||" + System.lineSeparator() );
-
-			perguntasERespostas = new ArrayList<>();
-			while (scanner.hasNext()) {
-				perguntasERespostas.add( new PerguntaResposta(scanner.next(), scanner.next()) );
-			}
-
-		} catch (FileNotFoundException e) {
+			carregaPerguntas();
+			iniciaQuestionario();
+			perguntaRespostas.forEach(System.out::println);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		perguntasERespostas.forEach(System.out::println);
+	}
+	
+	private void carregaPerguntas() throws FileNotFoundException {
+		Scanner scanner = new Scanner( new File("formulario.txt") );
+		// Usa o '|' e a quebr de linha como delimitador
+		scanner.useDelimiter( "\\||" + System.lineSeparator() );
+
+		perguntaRespostas = new ArrayList<>();
+		while (scanner.hasNext()) {
+			perguntaRespostas.add( new PerguntaResposta(scanner.next(), scanner.next()) );
+		}
+	}
+	
+	private void iniciaQuestionario() throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		for (PerguntaResposta perguntaResposta : perguntaRespostas) {
+			System.out.println(perguntaResposta.getPergunta());
+			perguntaResposta.setResposta(reader.readLine());
+		}
 	}
 
 }
