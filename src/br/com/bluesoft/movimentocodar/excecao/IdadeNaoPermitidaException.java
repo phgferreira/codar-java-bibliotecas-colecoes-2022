3813e5000421916bc35dd6a16a89bf85966237ed
Paulo Ferreira <phgferreira@outlook.com>
2022-09-17 00:05:24 -0300

    Serializando a excessão IdadeNaoPermitidaException

--- src/br/com/bluesoft/movimentocodar/excecao/IdadeNaoPermitidaException.java
+++ src/br/com/bluesoft/movimentocodar/excecao/IdadeNaoPermitidaException.java
@@ -2,6 +2,11 @@ package br.com.bluesoft.movimentocodar.excecao;
 
 public class IdadeNaoPermitidaException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public IdadeNaoPermitidaException(String msg) {
 		super(msg);
 	}
