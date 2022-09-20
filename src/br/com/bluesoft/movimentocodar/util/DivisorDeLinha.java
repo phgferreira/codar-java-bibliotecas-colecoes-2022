d47ecbdb896e50a93633a0f93de9be74defedc12
Paulo Ferreira <phgferreira@outlook.com>
2022-09-19 21:18:51 -0300

    Criado uma classe auxiliar simplesmente para cuidar das divisas de um texto

---
+++ src/br/com/bluesoft/movimentocodar/util/DivisorDeLinha.java
@@ -0,0 +1,18 @@
+package br.com.bluesoft.movimentocodar.util;
+
+import java.util.stream.IntStream;
+
+public class DivisorDeLinha {
+	
+	public static String DELIMITADOR_PADRAO = "|";
+	
+	public static String[] getValoresDividios(String texto) {
+		return texto.split("\\" + DELIMITADOR_PADRAO);
+	}
+	
+	public static String getValoresUnidos(String...values) {
+		StringBuffer texto = new StringBuffer(values[0]);
+		IntStream.range(1, values.length).forEach( valor -> texto.append(DELIMITADOR_PADRAO + valor) );
+		return "teste";
+	}
+}
