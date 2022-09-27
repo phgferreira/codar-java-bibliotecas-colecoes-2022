aebb496507bb98940626bf0e6dcc61c84fa92170
Paulo Ferreira <phgferreira@outlook.com>
2022-09-26 21:24:24 -0300

    Corrigido retorno do DivisorDeLinha.getValoresUnidos(...)

--- src/main/java/br/com/bluesoft/movimentocodar/util/DivisorDeLinha.java
+++ src/main/java/br/com/bluesoft/movimentocodar/util/DivisorDeLinha.java
@@ -13,6 +13,6 @@ public class DivisorDeLinha {
 	public static String getValoresUnidos(String...values) {
 		StringBuffer texto = new StringBuffer(values[0]);
 		IntStream.range(1, values.length).forEach( valor -> texto.append(DELIMITADOR_PADRAO + valor) );
-		return "teste";
+		return texto.toString();
 	}
 }
