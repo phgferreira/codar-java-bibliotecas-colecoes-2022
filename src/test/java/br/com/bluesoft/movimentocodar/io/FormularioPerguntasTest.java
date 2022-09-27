2f59f8c0cd2d5fee4c9135f64b2dc6062f567a38
Paulo Ferreira <phgferreira@outlook.com>
2022-09-26 21:15:29 -0300

    Adicionado teste de lista de perguntas

--- src/test/java/br/com/bluesoft/movimentocodar/io/FormularioPerguntasTest.java
+++ src/test/java/br/com/bluesoft/movimentocodar/io/FormularioPerguntasTest.java
@@ -1,14 +1,27 @@
 package br.com.bluesoft.movimentocodar.io;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import br.com.bluesoft.movimentocodar.modelo.Pergunta;
 
 class FormularioPerguntasTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void devePegarAsPerguntasDoFormularioEmLista() {
+		try {
+			FormularioPerguntas formularioPerguntas = new FormularioPerguntas();
+			List<Pergunta> perguntas = formularioPerguntas.getPerguntasEmLista();
+			assertFalse(perguntas.isEmpty());
+		} catch (IOException e) {
+			fail();
+		}
+		
 	}
 
 }
