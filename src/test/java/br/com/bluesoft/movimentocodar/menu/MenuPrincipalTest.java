45484104e1a9635cdca14a5c71bf69a14bfc168d
Paulo Ferreira <phgferreira@outlook.com>
2022-09-27 19:26:05 -0300

    Validado exibição do menu com opções de submenu

--- src/test/java/br/com/bluesoft/movimentocodar/menu/MenuPrincipalTest.java
+++ src/test/java/br/com/bluesoft/movimentocodar/menu/MenuPrincipalTest.java
@@ -1,14 +1,18 @@
 package br.com.bluesoft.movimentocodar.menu;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -25,24 +29,34 @@ class MenuPrincipalTest {
 	
 	@Mock
 	private MenuTest menuTest;
-
-
-	private ArgumentCaptor<String> captorDeTitulo;
-	private ArgumentCaptor<List<Menu>> captorDeOpcoes;
+	
+	private static ByteArrayOutputStream tela;
+	
+	@BeforeAll
+	static void beforeAll() {
+		tela = new ByteArrayOutputStream();
+		System.setOut( new PrintStream(tela) );
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		try {
+			tela.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@BeforeEach
 	void beforeEach() {
 		MockitoAnnotations.openMocks(this);
-		
-		//opcoes = new OpcoesSubmenu( interfaceUsuario );
-		
 		menuPrincipal = new MenuPrincipal(interfaceUsuario, montaListaMenu());
 	}
 	
 	@Test
 	void deveExibirMenuEPegarEscolhaDoUsuarioComOpcaoValida() {
 		try {
-			Mockito.when(interfaceUsuario.getRespostaEmInteiro()).thenReturn(0);
+			Mockito.when(interfaceUsuario.getRespostaEmInteiro()).thenReturn(2);
 		} catch (NumberFormatException | IOException e) {
 			fail(e.getMessage());
 			e.printStackTrace();
@@ -51,27 +65,26 @@ class MenuPrincipalTest {
 		// Elemento teste
 		menuPrincipal.abreMenu();
 		
-//		Mockito.when(opcoes.getOpcoes()).thenReturn(montaListaMenu());
-//		Mockito.when(opcoes.getOpcao( Mockito.anyInt() )).thenReturn(new MenuSair(interfaceUsuario, saida));
-//
-//		menuPrincipal = new MenuPrincipal(interfaceUsuario, saida, opcoes);
-//		System.out.println("Início do teste");
-//		menuPrincipal.abreMenu();
-//		System.out.println("Fim do teset");
-//		
-//		Mockito.verify(saida).exibeMensagem(captorDeTitulo.capture());
-//		String titulo = captorDeTitulo.getValue();
-//		assertEquals("### " + menuPrincipal.getTitulo() + " ###", titulo);
-//		
-//		Mockito.verify(saida).exibeOpcoesDeMenu(captorDeOpcoes.capture());
-//		List<Menu> opcoes = captorDeOpcoes.getValue();
-//		assertFalse(opcoes.isEmpty());
-//		assertTrue( opcoes.get(0) instanceof MenuSair );
+		assertEquals( exibidoEmTela(), tela.toString() );
 		
+	}
+	
+	private String exibidoEmTela() {
+		return	"### Menu Principal ###" + System.lineSeparator()
+				+ "0 - Menu de Teste" + System.lineSeparator()
+				+ "1 - Menu de Teste" + System.lineSeparator()
+				+ "2 - Menu de Teste" + System.lineSeparator()
+				+ "3 - Menu de Teste" + System.lineSeparator()
+				+ "4 - Menu de Teste" + System.lineSeparator()
+				+ "Abriu menu de teste" + System.lineSeparator();
 	}
 	
 	private List<Menu> montaListaMenu() {
 		return Arrays.asList(
+				new MenuTest(),
+				new MenuTest(),
+				new MenuTest(),
+				new MenuTest(),
 				new MenuTest()
 			);
 	}
