47163ac43cacc848810ca4d10dcc65dfd07719c7
Paulo Ferreira <phgferreira@outlook.com>
2022-09-28 23:47:46 -0300

    Continuando teste do MenuCandidatarSe

--- src/test/java/br/com/bluesoft/movimentocodar/menu/MenuCandidatarSeTest.java
+++ src/test/java/br/com/bluesoft/movimentocodar/menu/MenuCandidatarSeTest.java
@@ -1,16 +1,38 @@
 package br.com.bluesoft.movimentocodar.menu;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import br.com.bluesoft.movimentocodar.io.InterfaceUsuario;
 
 class MenuCandidatarSeTest {
 	
 	private MenuCandidatarSe menu;
 
+	@Mock
+	private InterfaceUsuario interfaceUsuario;
+
+	private static ByteArrayOutputStream console;
+
+	@BeforeEach
+	void beforeEach() {
+		MockitoAnnotations.openMocks(this);
+		menu = new MenuCandidatarSe(interfaceUsuario);
+		System.setOut( new PrintStream( console ) );
+	}
+	
 	@Test
 	void deveAbrirMenu() {
-		fail("Not yet implemented");
+		
+		menu.abreMenu();
+		
+		console.toString();
+		
 	}
 
 }
